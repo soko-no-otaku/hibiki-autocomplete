5387c8f2dd4f21a07cdebdf7e6a80f42756f2552
soko-no-otaku <soko.no.otaku@gmail.com>
2016-12-22 19:55:58 +0900

    init

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+document.getElementById("inquiry_name").value = "しいたけ";
+document.getElementById("inquiry_email").value = "kankanmikan@tochiman.com";
+document.getElementById("inquiry_gender").getElementsByTagName("option")[2].selected = true;
+document.getElementById("inquiry_generation").value = "高校2年生";
+document.getElementById("inquiry_area").value = "内浦";
